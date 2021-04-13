8e04cd1675c4c82c7f4b74400b8689bfbfe917dd
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2021-04-13 10:17:59 +0900

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+  # 新規登録
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [
       # 病院・利用者側
@@ -21,6 +22,25 @@ class ApplicationController < ActionController::Base
       
       service_type: [],
     ])
-  end
 
-end
+    # 編集
+    devise_parameter_sanitizer.permit(:account_update, keys: [
+      # 病院・利用者側
+      :main_company_name,
+      :sub_company_name,
+      :postal_code,
+      :address_city,
+      :address_street,
+      :address_building,
+      :human_name,
+      :phone_number,
+      :email,
+      
+      # 事業所側
+      :company_name,
+      :name,
+      
+      service_type: [],
+    ])
+    end
+  end
