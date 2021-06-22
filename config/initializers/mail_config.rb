6a87071fe5b80551304ba270108b7cd5f614f6aa
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2021-06-22 14:57:53 +0900

    test

--- config/initializers/mail_config.rb
+++ config/initializers/mail_config.rb
@@ -1,24 +1,10 @@
-# ActionMailer::Base.delivery_method = :smtp
-# ActionMailer::Base.smtp_settings = {
-#   address: 'smtp.gmail.com',
-#   domain: 'gmail.com',
-#   port: 587,
-#   user_name: ENV['gmail_address'],
-#   password: ENV['gmail_password'],
-#   authentication: 'plain',
-#   enable_starttls_auto: true
-# }
-
-mail = ENV['gmail_address']
-pass = ENV['gmail_application_password']   
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+  address: 'smtp.gmail.com',
+  domain: 'gmail.com',
   port: 587,
-  domain: "smtp.gmail.com",
-  authentication: "plain",
-  user_name: mail,
-  password: pass,
+  user_name: ENV['gmail_address'],
+  password: ENV['gmail_application_password'],
+  authentication: 'plain',
   enable_starttls_auto: true
 }
