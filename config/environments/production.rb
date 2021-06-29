6cb1efa8ebc40ac27190d95144477b82496bdb02
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2021-06-29 11:14:13 +0900

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,10 +64,10 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.send_mail_settings = { ... }
+  config.action_mailer.raise_delivery_errors = true
+  
+  config.action_mailer.delivery_method = :sendmail
   host = 'https://iryofuku.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
