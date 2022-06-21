31e50d1475f5081e7f2edf93c59af2e7ec990e14
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-06-21 12:41:13 +0900

    ✨viewsの作成

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from . import serializers
+from .models import Service
 
-# Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = serializers.UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class ServiceViewSet(viewsets.ModelViewSet):
+    queryset = Service.objects.all()
+    serializer_class = serializers.ServiceSerializer
