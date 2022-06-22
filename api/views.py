327cef922fca63a1eace1e9ce0cd45959d425025
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-06-22 13:09:00 +0900

    ✨viewsの追加

--- api/views.py
+++ api/views.py
@@ -1,14 +1,24 @@
 from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
+from .models import User, UserService, Iryo, IryoAddress
+
+from api.models import UserService
 from . import serializers
-from .models import Service
 
 class CreateUserView(generics.CreateAPIView):
     serializer_class = serializers.UserSerializer
     permission_classes = (AllowAny,)
 
+class UserServiceViewSet(viewsets.ModelViewSet):
+    queryset = UserService.objects.all()
+    serializer_class = serializers.UserServiceSerializer
+    permission_classes = (AllowAny,)
 
-class ServiceViewSet(viewsets.ModelViewSet):
-    queryset = Service.objects.all()
-    serializer_class = serializers.ServiceSerializer
+class IryoViewSet(viewsets.ModelViewSet):
+    queryset = Iryo.objects.all()
+    serializer_class = serializers.IryoSerializer
+    
+class IryoAddressViewSet(viewsets.ModelViewSet):
+    queryset = IryoAddress.objects.all()
+    serializer_class = serializers.IryoAddressSerializer
